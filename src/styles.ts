6f1b8fda3d1d03b04b017ff4c02a067e9e737ae9
Cosmas Augustino Billa <augubilla100@gmail.com>
2021-03-24 09:33:07 +0300

    create styled components

--- src/styles.ts
+++ src/styles.ts
@@ -1,5 +1,43 @@
-import styled from "styled-components";
+import styled, { keyframes } from "styled-components";
 
 export const AppContainer = styled.div`
-	
+	text-align: center;
+`;
+
+export const HeaderWrapper = styled.header`
+	background-color: #282c34;
+	min-height: 100vh;
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+	justify-content: center;
+	font-size: calc(10px + 2vmin);
+	color: white;
+`;
+
+export const spin = keyframes`
+	from {
+		transform: rotate(0deg);
+	}
+	to {
+		transform: rotate(360deg);
+	}
+`;
+
+export const LogoWrapper = styled.img`
+	height: 40vmin;
+	pointer-events: none;
+	animation: ${spin} infinite 20s linear;
+`;
+
+export const AppLink = styled.a`
+	color: #61dafb;
+`;
+
+export const Content = styled.p`
+	color: white;
+`;
+
+export const CodeWrapper = styled.code`
+	color: red;
 `;
